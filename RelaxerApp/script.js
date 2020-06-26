3171a35376a7561826588dcdbb5274c706b8a005
jay055 <nwachukwuuche@gmail.com>
2020-06-26 19:58:10 +0200

    Added Relaxer App

---
+++ RelaxerApp/script.js
@@ -0,0 +1,35 @@
+
+// Get DOM elements
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+
+const totalTime = 7500; 
+const breatheTime = (totalTime / 5 ) * 2 ;
+const holdTime = totalTime / 5;
+
+
+breatheAnimation();
+
+function breatheAnimation() { 
+  text.innerText = 'Breathe In'; 
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold'; 
+
+      setTimeout(() => {
+        text.innerText = 'Breathe Out'; 
+        container.className = 'container shrink'
+      }, holdTime);
+    }, breatheTime);
+  
+}
+
+
+// Don't stop
+setInterval(breatheAnimation, totalTime);
+
+
+
+ 
